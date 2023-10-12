57ed0d97e596e2ee3a21d3edbd962804b95068a8
Rafael-Silva-Oliveira <rafael.slo@outlook.com>
2023-10-12 09:49:33 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A DESeq Pipeline run in python for pre-processing, differential expression, GEAS, plotting and more.',
+    author='Rafael Oliveira, PhD',
+    license='MIT',
+)
